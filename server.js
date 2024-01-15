54eb3329fe9d557eb94585318fae784078980970
Samuel <salvadomo@hotmail.com>
2024-01-15 13:38:05 -0700

    modules/server port

---
+++ server.js
@@ -0,0 +1,8 @@
+// Required modules
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+// Setup server port
+const PORT = process.env.PORT || 3001;
+const app = express();
