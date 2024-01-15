3fca73c285e9a3572d2ed6465d40482c23965b48
Samuel <salvadomo@hotmail.com>
2024-01-15 13:41:18 -0700

    middleware

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+// Load initial note data
+const { notes } = require("./db/db.json");
+
 // Setup server port
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+
